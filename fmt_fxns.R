13fddd4f1a5c7f14d4e7108c42419387f8225400
paulhegedus <paulhegedus@montana.edu>
2022-07-04 16:11:08 -0600

    v0.0.6
    finished exploration and preparation of data. TODO = statistical analysis and modeling to determine what most influences each metric for each comm type.

--- fmt_fxns.R
+++ fmt_fxns.R
@@ -54,6 +54,57 @@ fixDayDateTime <- function(gsheet) {
                "6 PM", "7 PM", "8 PM", "9 PM", "10 PM", "11 PM")
   )
   
+  ## make day and hour combination
+  hour_f <- c("12 AM", "1 AM", "2 AM", "3 AM", "4 AM", "5 AM", 
+              "6 AM", "7 AM", "8 AM", "9 AM", "10 AM", "11 AM", 
+              "12 PM", "1 PM", "2 PM", "3 PM", "4 PM", "5 PM", 
+              "6 PM", "7 PM", "8 PM", "9 PM", "10 PM", "11 PM")
+  day_f <- c("Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday")
+  temp <- as.list(day_f) %>% 
+    lapply(function(x, hour_f) paste(x, hour_f), hour_f) %>% 
+    unlist()
+  gsheet$day_hour <- paste(gsheet$weekday_f, gsheet$hour_f) %>% 
+    factor(levels = temp)
+  
+  return(gsheet)
+}
+
+
+## calcFreq : Calculate frequency of email/post across month, week, day
+## Args : Google Sheet Import from '2022 Communications Metrics' that 
+##        has been run through the fixDayDateTime() function.
+## Method : Creates columns to calculate the following; 
+##          Create 'month_freq' = Number of email/posts that month.
+##          Create 'week_freq' = Number of email/posts that week.
+##          Create 'day_freq' = Number of email/posts that day.
+## Output : Google Sheet with appended columns.
+calcFreq <- function(gsheet) {
+  gsheet$id <- 1:nrow(gsheet)
+  # calculate the number of emails/posts in a month
+  temp <- by(gsheet, gsheet$month_f, nrow) %>% lapply(function(x) x[1]) %>% unlist() %>% as.data.frame()
+  names(temp) <- "month_freq"
+  temp$month <- row.names(temp)
+  temp2 <- merge(gsheet, temp, by = "month")[, c("id", "month_freq")]
+  gsheet$month_freq <- NA
+  gsheet[temp2$id, "month_freq"] <- temp2$month_freq
+  
+  # calculate the number of emails/posts in a week
+  gsheet$week <- strftime(gsheet$Date, format = "%V")
+  temp <- by(gsheet, gsheet$week, nrow) %>% lapply(function(x) x[1]) %>% unlist() %>% as.data.frame()
+  names(temp) <- "week_freq"
+  temp$week <- row.names(temp)
+  temp2 <- merge(gsheet, temp, by = "week")[, c("id", "week_freq")]
+  gsheet$week_freq <- NA
+  gsheet[temp2$id, "week_freq"] <- temp2$week_freq
+
+  # calculate the number of emails/posts in a day
+  temp <- by(gsheet, gsheet$usa_date, nrow) %>% lapply(function(x) x[1]) %>% unlist() %>% as.data.frame()
+  names(temp) <- "day_freq"
+  temp$usa_date <- row.names(temp)
+  temp2 <- merge(gsheet, temp, by = "usa_date")[, c("id", "day_freq")]
+  gsheet$day_freq <- NA
+  gsheet[temp2$id, "day_freq"] <- temp2$day_freq
+  
   return(gsheet)
 }
 
